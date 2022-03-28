361ac1b2d2152e719c4003c35834e89cd36d5b73
조용구 <younygo1004@gmail.com>
2022-03-29 00:03:18 +0900

    feat : User 관련 API 추가 #S06P22B102-348
    - user 관련 API 일부 기능 구현

--- spring boot/buur/src/main/java/com/bigdata/buur/controller/UserController.java
+++ spring boot/buur/src/main/java/com/bigdata/buur/controller/UserController.java
@@ -1,17 +1,14 @@
 package com.bigdata.buur.controller;
 
+import com.bigdata.buur.dto.SurveyDto;
 import com.bigdata.buur.dto.UserDto;
-import com.bigdata.buur.enums.UserRole;
 import com.bigdata.buur.enums.UserStatus;
 import com.bigdata.buur.service.UserService;
-import com.bigdata.buur.util.JwtTokenProvider;
-import com.bigdata.buur.entity.User;
-import com.bigdata.buur.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
@@ -21,15 +18,20 @@ public class UserController {
 
     private final UserService userService;
 
+    private final String SUCCESS = "success";
+    private final String FAIL = "fail";
+
     // 아이디 중복 체크
     @GetMapping("/id-check/{user_id}")
     public ResponseEntity<Boolean> checkIdDuplicate (@PathVariable String user_id){
+        if (user_id == null) ResponseEntity.ok().body(Boolean.FALSE);
         return ResponseEntity.ok(userService.checkIdDuplicate(user_id));
     }
 
     // 닉네임 중복 체크
     @GetMapping("/name-check/{user_nickname}")
     public ResponseEntity<Boolean> checkNicknameDuplicate (@PathVariable String user_nickname){
+        if (user_nickname == null) return ResponseEntity.ok().body(Boolean.FALSE);
         return ResponseEntity.ok(userService.checkNicknameDuplicate(user_nickname));
     }
 
@@ -39,20 +41,42 @@ public class UserController {
     // 회원가입
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody UserDto user) {
-        return ResponseEntity.ok().body(userService.addUser(user));
+        if(user == null || userService.addUser(user) == null)
+            return ResponseEntity.ok().body(FAIL);
+        else
+            return ResponseEntity.ok().body(SUCCESS);
     }
 
     // 로그인
     @PostMapping("/login")
-    public String login(@RequestBody UserDto user) {
-        return userService.login(user);
+    public ResponseEntity<String> login(@RequestBody UserDto user) {
+
+        if (user == null) return ResponseEntity.ok().body(FAIL);
+
+        try {
+            return ResponseEntity.ok().body(userService.login(user));
+        }
+        catch (Exception e){
+            return ResponseEntity.ok().body(FAIL);
+        }
+
     }
 
     // 신규회원 체크
     @GetMapping("/user/status/{user_id}")
-    public String userStatusDetails(@PathVariable String user_id) {
+    public ResponseEntity<String> userStatusDetails(@PathVariable String user_id) {
 
-        return null;
+        if(user_id == null) return ResponseEntity.ok().body(FAIL);
+
+        return ResponseEntity.ok().body(userService.findUserStatus(user_id));
+    }
+
+    @PostMapping("/user/survey")
+    public ResponseEntity<String> surveyAdd(@RequestBody List<SurveyDto> surveyDtoList) {
+
+//        if (surveyDtoList.isEmpty() || userService.) return ResponseEntity.ok().body(FAIL);
+
+        return ResponseEntity.ok().body(SUCCESS);
     }
 
     @Deprecated
