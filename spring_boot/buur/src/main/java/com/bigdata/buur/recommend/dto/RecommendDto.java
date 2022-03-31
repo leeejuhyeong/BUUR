52f08f64fc71e9fb6711b60897379d00869734e2
leeejuhyeong <wnguddl827@gmail.com>
2022-03-31 23:09:44 +0900

    fix : swagger 추가

--- spring_boot/buur/src/main/java/com/bigdata/buur/recommend/dto/RecommendDto.java
+++ spring_boot/buur/src/main/java/com/bigdata/buur/recommend/dto/RecommendDto.java
@@ -1,15 +1,23 @@
 package com.bigdata.buur.recommend.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Getter @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel("추천 맥주")
 public class RecommendDto {
+    @ApiModelProperty("맥주 번호")
     private Long beer_no;
+    @ApiModelProperty("맥주 이미지 경로")
     private String beer_image;
+    @ApiModelProperty("맥주 이름")
     private String beer_name;
+    @ApiModelProperty("맥주 좋아요")
     private boolean likes;
+    @ApiModelProperty("맥주 이미지")
     private byte[] beerImage;
 }
