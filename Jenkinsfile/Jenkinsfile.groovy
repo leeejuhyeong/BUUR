7083a99e1d6c70702a6b0bbb9609390853eb1556
leeejuhyeong <wnguddl827@gmail.com>
2022-04-04 00:08:12 +0900

    docs : docker run

--- Jenkinsfile/Jenkinsfile.groovy
+++ Jenkinsfile/Jenkinsfile.groovy
@@ -11,8 +11,25 @@ pipeline {
 		stage('Docker build') {
 			agent any
 			steps {
-				sh 'docker build -t backend:latest /var/jenkins_home/workspace/gitlab-hook/spring_boot/buur' 
+				sh 'docker build -t spring:latest /var/jenkins_home/workspace/gitlab-hook/spring_boot/buur' 
 			}
 		}
+		stage('Docker run') {
+			agent any
+			steps {
+				sh 'docker ps -f name=spring -q \
+		| xargs --no-run-if-empty docker container stop'
+
+				sh 'docker container ls -a -f name=spring -q \
+		| xargs -r docker container rm'
+
+				sh 'docker images -f dangling=true && \ 		
+				docker rmi $(docker images -f dangling=true -q)' 	
+
+				sh 'docker run -d --name spring \
+		--network jenkinsnetwork spring:latest'
+			}
+		}
+		
 	}
 }
