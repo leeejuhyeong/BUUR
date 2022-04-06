fd5227d11f1562328d87fed990a0fe46cc2d7306
leeejuhyeong <wnguddl827@gmail.com>
2022-04-06 21:25:30 +0900

    fix : 잠재요인분석 유저 id, 행번호 수정

--- django/beers/latent_factor.py
+++ django/beers/latent_factor.py
@@ -32,9 +32,12 @@ def get_data(buur_db):
     return review, beer
 
 
-def latent_factor_recommend(review):
+def latent_factor_recommend(review, id):
     df_user_beer_ratings = review.pivot_table(
         'total_score', index='id', columns='beer_id').fillna(0)
+    
+    # id에 해당하는 행번호를 알기 위한 df
+    df_userid_to_rownum = df_user_beer_ratings.reset_index().rename(columns={"index":"id"})
 
     # matrix는 pivot table(df_user_beer_rating)값을 matrix로 만든 것
     matrix = df_user_beer_ratings.to_numpy()
@@ -43,7 +46,6 @@ def latent_factor_recommend(review):
     # matrix_uesr_mean는 사용자-영화에 대해 평균평점을 뺀 값
     matrix_user_mean = matrix - user_rating_mean.reshape(-1, 1)
 
-    print(matrix_user_mean)
     U, sigma, Vt = svds(matrix_user_mean, k=2)
     sigma = np.diag(sigma)
 
@@ -53,16 +55,16 @@ def latent_factor_recommend(review):
         np.dot(U, sigma), Vt) + user_rating_mean.reshape(-1, 1)
 
     return pd.DataFrame(
-        svd_user_predicted_ratings, columns=df_user_beer_ratings.columns)
+        svd_user_predicted_ratings, columns=df_user_beer_ratings.columns), df_userid_to_rownum[df_userid_to_rownum['id'] == id].index[0]
 
 
-def recommend_beers(df_svd_preds, id, ori_beer_df, ori_ratings_df, num_recommendations=5):
+def recommend_beers(row_num, df_svd_preds, id, ori_beer_df, ori_ratings_df, num_recommendations=5):
 
     # userNumber를 index로 나타내기
-    user_row_number = id - 1
+    # user_row_number = id - 1
 
     # 최종적으로 만든 pred_df에서 사용자 index에 따라 맥주 데이터 정렬 -> 맥주 평점이 높은 순으로 정렬
-    sorted_user_predictions = df_svd_preds.iloc[user_row_number].sort_values(
+    sorted_user_predictions = df_svd_preds.loc[row_num].sort_values(
         ascending=False)
 
     # 원본 평점 데이터에서 user id에 해당하는 데이터를 뽑아냄
@@ -80,7 +82,7 @@ def recommend_beers(df_svd_preds, id, ori_beer_df, ori_ratings_df, num_recommend
         sorted_user_predictions).reset_index(), on='beer_id')
 
     recommendations = recommendations.rename(
-        columns={user_row_number: 'Predictions'}).sort_values('Predictions', ascending=False).head(num_recommendations)
+        columns={row_num: 'Predictions'}).sort_values('Predictions', ascending=False).head(num_recommendations)
 
     return user_history, recommendations
 
@@ -93,10 +95,29 @@ def use_latent(id):
     review, beer = get_data(buur_db)
 
     # 잠재 요소 기반 분석(SVD)
-    df_svd_preds = latent_factor_recommend(review)
+    df_svd_preds, row_num = latent_factor_recommend(review, id)     # 분석 정보, id에 해당하는 행번호
 
     # 추천
     alredady_rated, predictions = recommend_beers(
-        df_svd_preds, id, beer, review, 10)
+        row_num, df_svd_preds, id, beer, review, 10)
 
     return (predictions.to_dict('records'))
+
+
+if __name__=="__main__":
+    # db 연결
+    buur_db = connect()
+
+    # 데이터 가져오기
+    review, beer = get_data(buur_db)
+
+    # 잠재 요소 기반 분석(SVD)
+    df_svd_preds, row_num = latent_factor_recommend(review, 420)     # 분석 정보, id에 해당하는 행번호
+
+    # 추천
+    alredady_rated, predictions = recommend_beers(
+        row_num, df_svd_preds, 420, beer, review, 10)
+    
+    print("------------row_num------------")
+    print(row_num)
+    print(predictions.head(10))
